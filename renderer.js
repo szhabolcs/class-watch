e121e4cbac5c2c3f39e11294013c6cc9f9ab123a
Nagy Szabolcs <szabi.nagy.ns@gmail.com>
2020-07-30 20:11:56 +0300

    added more functionality to the front end

--- renderer.js
+++ renderer.js
@@ -10,11 +10,32 @@ const socket = io("http://gpcnet.go.ro:3000/");
 
   const { exec } = require('child_process');
   exec('gps | where {$_.MainWindowHandle -ne 0 } | select Description', {'shell':'powershell.exe'}, (error, stdout, stderr)=> {
-    console.log(stdout.split("\n"));
+    //console.log(stdout.split("\n"));
   });
 
+  function showModal(message){
+    $(".modal-body").text(message);
+    $("#warning-modal").modal("show");
+  }
+
+  /**
+   * Check wether the inputs are empty or not
+   * @param {function} callback callback function
+   */
+  function checkInputs(callback){
+    if($("#class-name").val() !== "" && $("#user-name").val() !== ""){
+      callback();
+    }
+    else{
+      showModal("Please fill in all the fields.");
+    }
+  }
+
 $("#enter-class").click(function(){
-    //TODO: enter class
+    checkInputs(()=>{
+      $("body").load("student.html");
+      //TODO: enter class
+    });
 });
 
 $("#create-class").click(function(){
